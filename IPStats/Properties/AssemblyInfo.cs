748a6f058a58d1ff57882e86cd895fdd0feaa97b
Simon Mourier <smourier@users.noreply.github.com>
2014-09-03 16:47:50 +0200

    Project initialization

---
+++ IPStats/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("IPStats")]
+[assembly: AssemblyDescription("Displays TCP connections statistics")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("SoftFluent S.A.S. http://www.softfluent.com")]
+[assembly: AssemblyProduct("IPStats")]
+[assembly: AssemblyCopyright("Copyright © 2014 Simon Mourier. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
